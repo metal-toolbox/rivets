be7e9afa17e2a02b20ee358a37b43eee605dede1
Joel Rebello <jrebello@packet.com>
2023-09-12 12:13:32 +0200

    serverservice: Add shared consts

---
+++ serverservice/consts.go
@@ -0,0 +1,12 @@
+package serverservice
+
+const (
+	// Serverservice attribute namespace for device vendor, model, serial attributes
+	ServerAttributeNSVendor = "sh.hollow.alloy.server_vendor_attributes"
+
+	// Serverservice attribute namespace for the BMC address
+	ServerAttributeNSBmcAddress = "sh.hollow.bmc_info"
+
+	// Serverservice attribute namespace for firmware set labels
+	FirmwareAttributeNSFirmwareSetLabels = "sh.hollow.firmware_set.labels"
+)
