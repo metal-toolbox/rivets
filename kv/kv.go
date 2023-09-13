e6f34b8e47b5096a26e0a7e771c49689fd8035b4
Joel Rebello <jrebello@packet.com>
2023-09-13 12:03:52 +0200

    Add Linter CI things and various shared types, methods (#1)
    * Add CI/linter configurations
    
    * Add renovate configuration, update golangCI config
    
    * Add shared types, methods
    
    * Add Makefile
    
    * kv: use method on type

---
+++ kv/kv.go
@@ -0,0 +1,168 @@
+package kv
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/metal-toolbox/flasher/types"
+	"github.com/metal-toolbox/rivets/condition"
+	"github.com/nats-io/nats.go"
+	"github.com/pkg/errors"
+	"go.hollow.sh/toolbox/events/registry"
+)
+
+type ErrQueryStatus struct {
+	err       error
+	kvBucket  string
+	lookupKey string
+	workerID  string
+}
+
+func (e *ErrQueryStatus) Error() string {
+	s := fmt.Sprintf("Condition status lookup error, bucket=%s", e.kvBucket)
+
+	if e.lookupKey != "" {
+		s += fmt.Sprintf(" lookupKey=%s", e.lookupKey)
+	}
+
+	if e.workerID != "" {
+		s += fmt.Sprintf(" workerID=%s", e.workerID)
+	}
+
+	if e.err != nil {
+		s += fmt.Sprintf(" err=%s", e.err.Error())
+	}
+
+	return s
+}
+
+func newErrQueryStatus(err error, kvBucket, lookupKey, workerID string) error {
+	return &ErrQueryStatus{
+		err:       err,
+		kvBucket:  kvBucket,
+		lookupKey: lookupKey,
+		workerID:  workerID,
+	}
+}
+
+const (
+	StatusValueVersion int32 = 1
+)
+
+// StatusValue is the canonical structure for reporting status of an ongoing task
+type StatusValue struct {
+	UpdatedAt       time.Time       `json:"updated"`
+	WorkerID        string          `json:"worker"`
+	Target          string          `json:"target"`
+	TraceID         string          `json:"traceID"`
+	SpanID          string          `json:"spanID"`
+	State           string          `json:"state"`
+	Status          json.RawMessage `json:"status"`
+	ResourceVersion int64           `json:"resourceVersion"` // for updates to server-service
+	MsgVersion      int32           `json:"msgVersion"`
+	// WorkSpec json.RawMessage `json:"spec"` XXX: for re-publish use-cases
+}
+
+// MustBytes sets the version field of the StatusValue so any callers don't have
+// to deal with it. It will panic if we cannot serialize to JSON for some reason.
+func (v *StatusValue) MustBytes() []byte {
+	v.MsgVersion = StatusValueVersion
+	byt, err := json.Marshal(v)
+	if err != nil {
+		panic("unable to serialize status value: " + err.Error())
+	}
+	return byt
+}
+
+func UnmarshalStatusValue(b []byte) (*StatusValue, error) {
+	sv := &StatusValue{}
+	if err := json.Unmarshal(b, &sv); err != nil {
+		return nil, err
+	}
+
+	return sv, nil
+}
+
+// TaskState holds the value for the task state in the KV.
+//
+// note: this differs from the condition.State, can these be merged?
+type TaskState int
+
+const (
+	NotStarted    TaskState = iota
+	InProgress              // another worker has started it, is still around and updated recently
+	Complete                // task is done
+	Orphaned                // the worker that started this task doesn't exist anymore
+	Indeterminate           // we got an error in the process of making the check
+)
+
+// ConditionStatus returns the status of the task from the KV store
+//
+//nolint:gocyclo // status checks are cyclomatic
+func ConditionStatus(conditionID, facilityCode, kvBucket string, js nats.JetStreamContext) (TaskState, error) {
+	handle, err := js.KeyValue(kvBucket)
+	if err != nil {
+		errKV := errors.Wrap(err, "bind to status KV bucket for condition lookup failed")
+		return Indeterminate, newErrQueryStatus(errKV, kvBucket, "", "")
+	}
+
+	lookupKey := fmt.Sprintf("%s.%s", facilityCode, conditionID)
+	entry, err := handle.Get(lookupKey)
+	switch err {
+	case nats.ErrKeyNotFound:
+		// This should be by far the most common path through this code.
+		return NotStarted, nil
+
+	case nil:
+		break // we'll handle this outside the switch
+
+	default:
+		return Indeterminate, newErrQueryStatus(err, kvBucket, lookupKey, "")
+	}
+
+	// we have an status entry for this condition, is is complete?
+	sv := types.StatusValue{}
+	if errJSON := json.Unmarshal(entry.Value(), &sv); errJSON != nil {
+		errJSON = errors.Wrap(errJSON, "unable to construct a sane status for condition")
+		return Indeterminate, newErrQueryStatus(errJSON, kvBucket, lookupKey, "")
+	}
+
+	if condition.StateIsComplete(condition.State(sv.State)) {
+		return Complete, nil
+	}
+
+	// is the worker handling this condition alive?
+	controllerID, err := registry.ControllerIDFromString(sv.WorkerID)
+	if err != nil {
+		errWorker := errors.Wrap(err, "bad worker ID")
+		return Indeterminate, newErrQueryStatus(errWorker, kvBucket, lookupKey, sv.WorkerID)
+	}
+
+	_, err = registry.LastContact(controllerID)
+	switch err {
+	case nats.ErrKeyNotFound:
+		// the data for this worker aged-out, it's no longer active
+		// XXX: the most conservative thing to do here is to return
+		// indeterminate but most times this will indicate that the
+		// worker crashed/restarted and this task should be restarted.
+
+		// We're going to restart this condition when we return from
+		// this function. Use the KV handle we have to delete the
+		// existing task key.
+		if err = handle.Delete(lookupKey); err != nil {
+			errWorker := errors.Wrap(err, "unable to delete existing condition status")
+
+			return Indeterminate, newErrQueryStatus(errWorker, kvBucket, lookupKey, sv.WorkerID)
+		}
+
+		return Orphaned, nil
+
+	case nil:
+		return InProgress, nil
+
+	default:
+		errWorker := errors.Wrap(err, "error looking up worker last contact")
+		return Indeterminate, newErrQueryStatus(errWorker, kvBucket, lookupKey, sv.WorkerID)
+	}
+}
---
+++ kv/kv_test.go
@@ -0,0 +1,195 @@
+package kv
+
+// nolint // test file
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/metal-toolbox/flasher/types"
+	"github.com/metal-toolbox/rivets/condition"
+	"github.com/nats-io/nats-server/v2/server"
+	srvtest "github.com/nats-io/nats-server/v2/test"
+	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.hollow.sh/toolbox/events"
+	"go.hollow.sh/toolbox/events/pkg/kv"
+	"go.hollow.sh/toolbox/events/registry"
+)
+
+func startJetStreamServer(t *testing.T) *server.Server {
+	t.Helper()
+	opts := srvtest.DefaultTestOptions
+	opts.Port = -1
+	opts.JetStream = true
+	return srvtest.RunServer(&opts)
+}
+
+func jetStreamContext(t *testing.T, s *server.Server) (*nats.Conn, nats.JetStreamContext) {
+	t.Helper()
+	nc, err := nats.Connect(s.ClientURL())
+	if err != nil {
+		t.Fatalf("connect => %v", err)
+	}
+	js, err := nc.JetStream(nats.MaxWait(10 * time.Second))
+	if err != nil {
+		t.Fatalf("JetStream => %v", err)
+	}
+	return nc, js
+}
+
+func shutdownJetStream(t *testing.T, s *server.Server) {
+	t.Helper()
+	var sd string
+	if config := s.JetStreamConfig(); config != nil {
+		sd = config.StoreDir
+	}
+	s.Shutdown()
+	if sd != "" {
+		if err := os.RemoveAll(sd); err != nil {
+			t.Fatalf("Unable to remove storage %q: %v", sd, err)
+		}
+	}
+	s.WaitForShutdown()
+}
+
+func TestConditionStatus(t *testing.T) {
+	srv := startJetStreamServer(t)
+	defer shutdownJetStream(t, srv)
+	nc, js := jetStreamContext(t, srv)
+	evJS := events.NewJetstreamFromConn(nc)
+	defer evJS.Close()
+
+	testKvBucket := "testBucket"
+	facilityCode := "test1"
+	conditionID := uuid.New()
+	workerID := "workerA"
+
+	workerRegistryID := registry.GetID(workerID)
+
+	key := fmt.Sprintf("%s.%s", facilityCode, conditionID.String())
+
+	// set up the fake status KV
+	cfg := &nats.KeyValueConfig{
+		Bucket: string(testKvBucket),
+	}
+
+	kvWriteHandle, err := js.CreateKeyValue(cfg)
+	require.NoError(t, err, "creating KV")
+
+	// nolint:govet // these tests get no fieldalignment because
+	tests := []struct {
+		name                 string
+		expectState          TaskState
+		expectErrorContains  string
+		statusValue          func() []byte
+		deregisterController bool
+	}{
+		{
+			"empty KV",
+			NotStarted,
+			"",
+			nil,
+			false,
+		},
+		{
+			"bad status value",
+			Indeterminate,
+			"unable to construct a sane status for condition",
+			func() []byte { return []byte(`non-status-value`) },
+			false,
+		},
+		{
+			"finished status value",
+			Complete,
+			"",
+			func() []byte {
+				sv := &types.StatusValue{State: string(condition.Failed)}
+				return sv.MustBytes()
+			},
+			false,
+		},
+		{
+			"bogus worker ID",
+			Indeterminate,
+			"bad worker ID",
+			func() []byte {
+				sv := &StatusValue{
+					State:    string(condition.Pending),
+					WorkerID: "some junk id",
+				}
+
+				return sv.MustBytes()
+			},
+			false,
+		},
+		{
+			"register test worker and lookup value",
+			InProgress,
+			"",
+			func() []byte {
+				// init registry and register controller
+				err = registry.InitializeRegistryWithOptions(evJS, kv.WithReplicas(1))
+				require.NoError(t, err, "initialize controller registry")
+
+				err = registry.RegisterController(workerRegistryID)
+				require.NoError(t, err, "register test controller")
+
+				sv := &StatusValue{
+					State:    string(condition.Pending),
+					WorkerID: workerRegistryID.String(),
+				}
+				return sv.MustBytes()
+			},
+			false,
+		},
+		{
+			"no live workers - condition Orphaned",
+			Orphaned,
+			"",
+			func() []byte {
+				// deregister controller
+				err = registry.DeregisterController(workerRegistryID)
+				require.NoError(t, err, "deregister controller")
+
+				sv := &StatusValue{
+					State:    string(condition.Pending),
+					WorkerID: workerRegistryID.String(),
+				}
+				return sv.MustBytes()
+			},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.statusValue != nil {
+				_, err = kvWriteHandle.Put(key, tt.statusValue())
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			gotState, err := ConditionStatus(conditionID.String(), facilityCode, testKvBucket, js)
+			if tt.expectErrorContains != "" {
+				assert.ErrorContains(t, err, tt.expectErrorContains)
+				return
+			}
+
+			assert.Nil(t, err)
+
+			// make sure that the Delete fired to clear the KV and make things clean for a new worker
+			if tt.deregisterController {
+				_, err = kvWriteHandle.Get(conditionID.String())
+				require.ErrorIs(t, err, nats.ErrKeyNotFound)
+			}
+
+			require.Equal(t, tt.expectState, gotState, tt.name)
+		})
+	}
+}
