dbb68198f3c25441bd7f8a397d34561db9f9d217
Joel Rebello <jrebello@packet.com>
2024-07-18 12:25:52 +0200

    events/controller: TaskHandler interface replaces ConditionHandler interface
    The Controller handler method is updated to pass in the Task type,
    instead of the Condition type, this allows the handler to deal with the
    Task type and leave the Condition handling to the library.

---
+++ events/controller/interface.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/metal-toolbox/rivets/condition"
+)
+
+// TaskHandler is passed in by the caller to be invoked when a message from the Jetstream is recieved for processing.
+type TaskHandler interface {
+	HandleTask(ctx context.Context, task *condition.Task[any, any], statusPublisher Publisher) error
+}
+
+type ConditionHandlerFactory func() TaskHandler
